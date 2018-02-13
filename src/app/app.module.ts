09ea9dfec65eb8d76dcc15da0bcec4ad680d12cd
vickry Miguel Sanatana Rincon <vickry.santana@gmail.com>
2018-02-12 21:54:48 -0400

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // rutas
 import { APP_ROUTES } from './app.routes';
 
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -22,19 +19,12 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    PagenotfoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,31 @@
 import {RouterModule, Routes} from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
+
 
 
 
 
 const appRoutes: Routes = [
     // primero creamos las rutas para las paginas
-    {
-        // este path sin ruta va a redireccionar al componente
-        path: '',
-        // el cual tiene HIJOS que son:
-        component: PagesComponent,
-        children: [
-            // estos:
-            {path: 'dashboard', component: DashboardComponent},
-            {path: 'progress', component: ProgressComponent},
-            {path: 'graficas1', component: Graficas1Component},
-            // si el path llega vacio, redirecciona al dashboard
-            {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    // {
+    //     // este path sin ruta va a redireccionar al componente
+    //     path: '',
+    //     // el cual tiene HIJOS que son:
+    //     component: PagesComponent,
+    //     children: [
+    //         // estos:
+    //         {path: 'dashboard', component: DashboardComponent},
+    //         {path: 'progress', component: ProgressComponent},
+    //         {path: 'graficas1', component: Graficas1Component},
+    //         // si el path llega vacio, redirecciona al dashboard
+    //         {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 
-        ]
-    },
+    //     ]
+    // },
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     // ruta cuando no exite ruta
